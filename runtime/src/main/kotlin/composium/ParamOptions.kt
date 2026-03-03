9ae0ecf59fb64de0ba03b9b6c800dc2cab01e115
oleginvoke <zelyoniyoleg@yandex.ru>
2026-03-03 14:15:20 +0500

    save

--- runtime/src/main/kotlin/composium/ParamOptions.kt
+++ runtime/src/main/kotlin/composium/ParamOptions.kt
@@ -13,9 +13,42 @@ data class ParamOption<out T>(
 
 @PublishedApi
 internal fun <T> List<ParamOption<T>>.checkFirstValue(): T = this.let { options ->
-    if(options.isEmpty()) {
+    if (options.isEmpty()) {
         error("Param options list is empty. Provide at least one option.")
     } else {
         options.first().value
+    }
+}
+
+@PublishedApi
+internal fun <T> List<ParamOption<T>>.ensureUniqueLabels(): List<ParamOption<T>> {
+    if (isEmpty()) return this
+
+    val originalLabels = map { option -> option.label }.toSet()
+    val usedLabels = hashSetOf<String>()
+
+    return map { option ->
+        val baseLabel = option.label
+        val resolvedLabel = if (usedLabels.add(baseLabel)) {
+            baseLabel
+        } else {
+            var suffix = 1
+            var candidate = baseLabel + suffix
+            while (candidate in usedLabels || candidate in originalLabels) {
+                suffix++
+                candidate = baseLabel + suffix
+            }
+            usedLabels.add(candidate)
+            candidate
+        }
+
+        if (resolvedLabel == option.label) {
+            option
+        } else {
+            ParamOption(
+                value = option.value,
+                label = resolvedLabel,
+            )
+        }
     }
 }
